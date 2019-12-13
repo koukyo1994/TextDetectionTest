230dd04693f41902320e88dc210dabf72112ea15
koukyo1994 <arabiannight1994@yahoo.co.jp>
2019-12-13 16:59:33 +0100

    Initial Commit

---
+++ TextDetectionTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextDetectionTest
+//
+//  Created by 荒居秀尚 on 13.12.19.
+//  Copyright © 2019 荒居秀尚. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
